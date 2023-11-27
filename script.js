f53f4b3144c21c12285a774d188538548e71694c
Aritra Arka Sarkar <116138104+arcturus987@users.noreply.github.com>
2023-11-27 01:33:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 var i = 0;
 
-const intervalId = setInterval(unsubscribeChannel, 500); //Execute the function every 500 milliseconds
-
 function unsubscribeChannel() {
-    const channelElements = Array.from(document.getElementsByTagName('ytd-channel-renderer')); // Find all channel elements on the page
+    const channelElements = Array.from(document.getElementsByTagName('ytd-channel-renderer')); // find all channel elements on the page
 
-    if (i < channelElements.length) { // Check if there are more channels to unsubscribe from
-        channelElements[i].querySelector("[aria-label^='Unsubscribe from']").click(); // Click the "Unsubscribe" button
+    if (i < channelElements.length) { // check if there are more channels to unsubscribe from
+        channelElements[i].querySelector("[aria-label^='Unsubscribe from']").click(); // click the unsubscribe button
 
-        setTimeout(() => { // Set a timeout to confirm the unsubscribe action
-            const confirmButton = document.getElementById('confirm-button').querySelector("[aria-label^='Unsubscribe'"); // "Unsubscribe" button in the confirmation dialog
-            confirmButton.click();
+        setTimeout(() => { // set a timeout to confirm the unsubscribe action
+            const confirmButton = document.getElementById('confirm-button').querySelector("[aria-label^='Unsubscribe']");
+            if (confirmButton) {
+                confirmButton.click();
+            } else {
+                console.error('Confirm button not found');
+            }
         }, 300);
+        console.log(i + ' unsubscribed'); // log the progress
+        i++;
+    } else {
+        console.log('No more channels to unsubscribe from.');
+        clearInterval(intervalId); // clear the interval to stop the function
     }
-
-    i++;
-
-    console.log(i + ' unsubscribed');  // Log the progress
     console.log(channelElements.length - i + ' remaining');
 }
 
-//function stopInterval() {
-//    clearInterval(intervalId);
-//}
-
-// Uncomment the line below to stop the interval after unsubscribing from all channels
-// stopInterval();
+const intervalId = setInterval(unsubscribeChannel, 500); //execute the function every 500 milliseconds
